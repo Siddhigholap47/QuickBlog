ea1189065bd48fcae959559538f6a95f9d7c9409
siddhi gholap <siddhigholap47@gmail.com>
2025-11-26 17:21:19 +0530

    fix: correct Newsletter import casing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
-import Newsletter from '../components/NewsLetter'
+import Newsletter from '../components/Newsletter'
 import Footer from '../components/Footer'
 
 
